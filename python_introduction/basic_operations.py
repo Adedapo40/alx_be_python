591111d82145171817a5fda5f3e139eac7044ad6
Adedapo Ayanyemi <123646741+Adedapo40@users.noreply.github.com>
2025-11-09 21:26:12 +0100

    basic arithmetic operations in basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+num1 = 23
+num2 = 4
+x = num1 + num2
+y = num1 - num2
+z = num1 * num2
+print ("Addition of num1 and num2 is", x);
+print ("Subtraction of num1 and num2 is", y);
+print ("Multiplication of num1 and num2 is", z);
