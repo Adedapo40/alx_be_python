a6bcd8c6c2063bd735987d227429883be2362061
Adedapo Ayanyemi <123646741+Adedapo40@users.noreply.github.com>
2025-11-09 21:28:17 +0100

    Rename variables for consistency in basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-num1 = 23
-num2 = 4
+number1 = 23
+number2 = 4
 x = num1 + num2
 y = num1 - num2
 z = num1 * num2
-print ("Addition of num1 and num2 is", x);
-print ("Subtraction of num1 and num2 is", y);
-print ("Multiplication of num1 and num2 is", z);
+print ("Addition of number1 and number2 is", x);
+print ("Subtraction of number1 and number2 is", y);
+print ("Multiplication of number1 and number2 is", z);
