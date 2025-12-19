b7a8f924941911823d32d8f4e9046c55adf96903
Adedapo Ayanyemi <123646741+Adedapo40@users.noreply.github.com>
2025-12-19 17:36:17 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
-def perform_operation():
-    operation = operation.lower().strip
+def perform_operation(num1, num2, operation):
+    operation = operation.lower()
     
     if operation == 'add':
         return num1 + num2
@@ -8,10 +8,8 @@ def perform_operation():
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        # Check for division by zero
         if num2 == 0:
             return "Error: Division by zero is not allowed."
         return num1 / num2
     else:
-        # Handle invalid operation
         return "Error: Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'."
